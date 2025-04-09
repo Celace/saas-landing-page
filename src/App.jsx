8b265e7c7ed57ac91100b90bafb0d9fff6abdd78
Selase <selasebredzei@gmail.com>
2025-04-09 09:15:15 +0000

    Testimonials section completed

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import Main from "./components/sections/Main";
 import Logos from "./components/sections/Logos";
 import Features from "./components/sections/Features";
 import FAQs from "./components/sections/FAQs/FAQs";
+import Testimonials from "./components/sections/testimonials/Testimonials";
 
 const App = () => {
   return (
@@ -28,6 +29,7 @@ const App = () => {
         <Logos />
         <Features />
         <FAQs />
+        <Testimonials />
       </Main>
     </Page>
   );
